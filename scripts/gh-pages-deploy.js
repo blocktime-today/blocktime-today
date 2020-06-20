51c56d47c83ee561fa68efc2920257af9457c256
Gigi <dergigi@pm.me>
2020-06-20 18:33:17 +0200

    Update gh-pages-deploy.js

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -6,9 +6,10 @@
       await execa("git", ["checkout", "--orphan", "gh-pages"]);
       console.log("Building...");
       await execa("npm", ["run", "build"]);
-      const folderName = fs.existsSync("dist") ? "dist" : "build";
-      await execa("git", ["--work-tree", folderName, "add", "--all"]);
-      await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+      // const folderName = fs.existsSync("dist") ? "dist" : "build";
+      await execa("bundle", ["exec", "jekyll", "build"]);
+      await execa("git", ["--work-tree", "_site", "add", "--all"]);
+      await execa("git", ["--work-tree", "_site", "commit", "-m", "gh-pages"]);
       console.log("Pushing to gh-pages...");
       await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
       await execa("rm", ["-r", folderName]);

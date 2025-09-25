5bc019d2d3db4c89118305a2efc8ea062381c045
dergigi <dergigi@pm.me>
2025-09-25 22:37:09 +0000

    gh-pages

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,23 @@
+  const fs = require("fs");
+
+  (async () => {
+    const { execa } = await import("execa");
+    try {
+      await execa("git", ["checkout", "--orphan", "gh-pages"]);
+      console.log("Building...");
+      await execa("npm", ["run", "build"]);
+      // const folderName = fs.existsSync("dist") ? "dist" : "build";
+      await execa("bundle", ["exec", "jekyll", "build"]);
+      await execa("git", ["--work-tree", "_site", "add", "--all"]);
+      await execa("git", ["--work-tree", "_site", "commit", "-m", "gh-pages"]);
+      console.log("Pushing to gh-pages...");
+      await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+      await execa("rm", ["-r", "_site"]);
+      await execa("git", ["checkout", "-f", "master"]);
+      await execa("git", ["branch", "-D", "gh-pages"]);
+      console.log("Successfully deployed");
+    } catch (e) {
+      console.log(e.message);
+      process.exit(1);
+    }
+  })();
